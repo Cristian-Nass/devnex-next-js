f069f591fb8430ecc6e219c98cd69dfa9a302937
Cristian Nass <cristian.tirdad@gmail.com>
2026-04-26 21:29:22 +0200

    save

--- components/main-sections/Products.tsx
+++ components/main-sections/Products.tsx
@@ -114,7 +114,7 @@ export default function Products() {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8">
           {products.map((product, index) => (
             <div
               key={product.title?.toString() || index}
