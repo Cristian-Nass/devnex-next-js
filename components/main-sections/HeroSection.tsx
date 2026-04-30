22d6cb1bca6bfe5f46e040813f370bb1937bfa33
Cristian Nass <cristian.tirdad@gmail.com>
2026-04-30 17:05:56 +0200

    save

--- components/main-sections/HeroSection.tsx
+++ components/main-sections/HeroSection.tsx
@@ -19,7 +19,7 @@ export default async function HeroSection({ locale }: HeroSectionProps) {
       className="w-full max-w-[1440px] p-10 mx-auto min-h-screen snap-start scroll-mt-24 flex"
       aria-labelledby="hero-heading"
     >
-      <div className="absolute right-1/10 top-1/2 -translate-y-1/2 h-auto w-1/3 flex justify-end pointer-events-none">
+      <div className="absolute right-1/10 top-1/2 -translate-y-1/2 h-auto w-1/3 flex justify-end pointer-events-none xl:opacity-100 opacity-20">
         <Image
           src="/hero-img.png"
           alt="Hero Image"
